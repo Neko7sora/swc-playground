1eed6601b067c9ec910f95eb9b59082105e352ba
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2022-05-25 15:27:24 +0900

    swc test

---
+++ src/test.ts
@@ -0,0 +1,23 @@
+var announcement = document.getElementById('announcement');
+
+if (announcement !== null) {
+
+  var id = announcement.dataset.id;
+
+  Object.keys(localStorage).forEach(function(key) {
+    if (/^global-alert-/.test(key)) {
+      if (key !== id ) {
+        localStorage.removeItem(key);
+        document.documentElement.removeAttribute('data-global-alert');
+      }
+    }
+  });
+
+  announcement.addEventListener('closed.bs.alert', () => {
+    localStorage.setItem(id, 'closed');
+  });
+
+}
+
+
+[1, 2, 3].map(n => n + 1);
